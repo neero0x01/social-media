5a1002d25e0213acce7bbe62cbc6f55476b3e39e
Muhammad Ahmad <181370123@gift.edu.pk>
2021-10-30 01:30:06 +0500

    add a container around post list with some shadow

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,7 +1,15 @@
 import Post from "./Post";
 
 const PostList = ({ posts }) => {
-  return posts.map((post, i) => <Post key={i} {...post} />);
+
+  return (
+      <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+        <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+          <h1 className="text-xl">Post Feed</h1>
+          {!(posts.length === 0) ? posts.map((post, i) => <Post key={i} {...post} />) : <h1 className="text-xl text-center">Feed is empty</h1>}
+        </div>
+      </div>
+  )
 }
 
 export default PostList;
