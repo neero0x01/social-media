26d72ed86e8bf0a70c20b124a511ae162b0a0341
Muhammad Ahmad <181370123@gift.edu.pk>
2021-10-30 01:29:40 +0500

    bump up image size

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -4,8 +4,8 @@ import {UserContext} from "../App";
 const Post = ({ content, image, user }) => {
   const currentUser = useContext(UserContext);
   const isCurrentUser =  currentUser === user
-  return (<div>
-  {image && <img className="w-1/6 h-1/6 object-cover" src={URL.createObjectURL(image)} alt="Post Image" />}
+  return (<div className="mb-4">
+  {image && <img className="w-1/2 h-1/2 object-cover" src={URL.createObjectURL(image)} alt="Post" />}
   <p>{ content }</p>
   <p className={isCurrentUser && "text-green-600 font-extrabold"}>{ user }</p>
 </div>);
