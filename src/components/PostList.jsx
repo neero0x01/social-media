25bcf2fc9c7f8628a3d9342385bf3e141682fd18
neero <181370123@gift.edu.pk>
2021-11-11 04:58:14 +0500

    use current date as id for post object

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -6,7 +6,7 @@ const PostList = ({ posts }) => {
       <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
         <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
           <h1 className="text-xl">Post Feed</h1>
-          {!(posts.length === 0) ? posts.map((post, i) => <Post key={i} {...post} />) : <h1 className="text-xl text-center">Feed is empty</h1>}
+          {!(posts.length === 0) ? posts.map((post) => <Post key={post.id} {...post} />) : <h1 className="text-xl text-center">Feed is empty</h1>}
         </div>
       </div>
   )
